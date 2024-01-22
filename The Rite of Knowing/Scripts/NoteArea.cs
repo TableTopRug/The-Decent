3e99d675909ac5c31aaf196fe2611113168ec3cb
GregCarterII <gcartcca2n@gmail.com>
2024-01-21 21:57:56 -0600

    updates

---
+++ The Rite of Knowing/Scripts/NoteArea.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class NoteArea : Area2D
+{
+	[Export]
+	public float Destination;
+	[Export]
+	public float change = 10;
+
+	private float start;
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		start = GlobalPosition.Y;
+		Destination = change - start;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Transform2D trans = GlobalTransform;
+
+		trans.Origin.Y = (float) Mathf.MoveToward(GlobalPosition.Y, Destination, delta * 1.7);
+
+		if (GlobalPosition.Y - Math.Abs(Destination) < .01) {
+			Destination = start + change;
+		}
+
+		GlobalTransform = trans;
+	}
+
+	private void OnBodyEntered(PhysicsBody2D body)
+	{
+		GD.Print("note get");
+		QueueFree();
+	}
+}
